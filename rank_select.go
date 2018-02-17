b6bda971aa1356b8afba1c63873180f845e7c9e3
Jesper Sindahl Nielsen <jesper.sindahl.nielsen@gmail.com>
2018-02-17 18:09:57 +0100

    comments for interface

--- rank_select.go
+++ rank_select.go
@@ -1,7 +1,13 @@
 package gorasp
 
+// interface for rank select structure on bit arrays.
 type RankSelect interface {
+	// return the number of 1-bits to the left of the index'th bit.
 	RankOfIndex(index int) uint
+
+	// return the lowest index j where RankOfIndex(j) = rank.
 	IndexWithRank(rank int) (int, error)
+
+	// return the bit at index.
 	At(index int) int
 }
